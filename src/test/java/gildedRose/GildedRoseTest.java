d306257b591b60e9baa1a47b6a7fc7edbaa4fc28
VincentHuang <vincent.huang@oocl.com>
2019-08-19 20:19:23 +0800

    [HGC]Add test : should_return_AgedBrie_0_50_when_invoke_updateQuality_given_AgedBrie_negative_1_50

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -13,4 +13,13 @@ public class GildedRoseTest {
         gildedRose.updateQuality();
         assertEquals("Aged Brie, 0, 50",gildedRose.items[0].toString());
     }
+
+    @Test
+    public void should_return_AgedBrie_0_50_when_invoke_updateQuality_given_AgedBrie_negative_1_50() {
+        Item item = new Item("Aged Brie",0,50);
+        Item[] items = {item};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals("Aged Brie, -1, 50",gildedRose.items[0].toString());
+    }
 }
