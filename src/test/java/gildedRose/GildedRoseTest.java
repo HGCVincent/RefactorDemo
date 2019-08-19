875b64d5c3acc9c0bf0df28234bba6f1bda55a4d
VincentHuang <vincent.huang@oocl.com>
2019-08-19 20:20:49 +0800

    [HGC]Add test : should_return_Backstage_1_50_when_invoke_updateQuality_given_Backstage_0_49()

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -31,4 +31,13 @@ public class GildedRoseTest {
         gildedRose.updateQuality();
         assertEquals("Aged Brie, -1, 50",gildedRose.items[0].toString());
     }
+
+    @Test
+    public void should_return_Backstage_1_50_when_invoke_updateQuality_given_Backstage_0_49() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert",1,49);
+        Item[] items = {item};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert, 0, 50",gildedRose.items[0].toString());
+    }
 }
